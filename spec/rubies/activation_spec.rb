efe46d6edf8d8ea1a98f310a937b7fe603f45092
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-02-18 16:33:59 -0800

    add pending specs

---
+++ spec/rubies/activation_spec.rb
@@ -0,0 +1,5 @@
+describe 'activation' do
+  it "replaces any previous activation's PATH entries"
+  it "sets gem environment variables"
+  it "sets current activated Ruby variables"
+end
---
+++ spec/rubies/deactivation_spec.rb
@@ -0,0 +1,4 @@
+describe "deactivation" do
+  it "removes the Ruby bin path from PATH"
+  it "removes the gem bin path from PATH"
+end
